ac059c0af213ba912e9628f0a81543761c8868bd
Arnav Surve <arnav.surve@milgard.com>
2024-07-26 14:31:32 -0700

    commented incorrect infrapy usage, updated sample mseed filepath, test obspy by printing stream

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ import infrapy.utils
 import matplotlib.pyplot as plt
 
 # read MiniSEED data
-stream = read('./datasets/Data_for_Forecasting_the_Eruption_of_an_Open_vent_Volcano_Using_Resonant_Infrasound_Tones_Johnson/VIC_miniSEED - Jeffrey B Johnson/2018-05-18T00-00-00Z.ms')
+stream = read('./datasets/Data_for_Forecasting_the_Eruption_of_an_Open_vent_Volcano_Using_Resonant_Infrasound_Tones_Johnson/VIC_miniSEED - Jeffrey B Johnson/VIC_02140000.ms')
 
-# filter data
-filtered_stream = infrapy.utils.filter_stream(stream, freqmin=0.5, freqmax=5.0)
+print(stream)
 
-# run detection
-
-detections = infrapy.detection.run_sta_lta(filtered_stream, sta=1, lta=10)
-
-# plot detections
-for trace in filtered_stream:
-    plt.plot(trace.times(), trace.data, label=trace.id)
-
-plt.legend()
-plt.show()
+# # filter data
+# filtered_stream = infrapy.utils.filter_stream(stream, freqmin=0.5, freqmax=5.0)
+#
+# # run detection
+#
+# detections = infrapy.detection.run_sta_lta(filtered_stream, sta=1, lta=10)
+#
+# # plot detections
+# for trace in filtered_stream:
+#     plt.plot(trace.times(), trace.data, label=trace.id)
+#
+# plt.legend()
+# plt.show()
